f2dad240bb89b2d51c2857ea585052eb30a9c6cb
Nicodragon25 <nicolasariellaure@gmail.com>
2023-10-13 11:10:27 -0300

    Updated Behaviour after player goes out of screen bounds

--- src/Objects/Player.cpp
+++ src/Objects/Player.cpp
@@ -6,6 +6,9 @@ namespace asteroids
 {
 	static const float BASE_SCREEN_WIDTH = 1280.0f;
 
+	void CheckScreenBoundsCollision(Player& player, float screenWidth, float screenHeight);
+
+
 	void PlayerUpdate(Player& player)
 	{
 		float screenWidth = static_cast<float>(GetScreenWidth());
@@ -16,19 +19,10 @@ namespace asteroids
 
 		if (IsMouseButtonDown(1))
 			player.velocity = Vector2Clamp(Vector2Add(player.velocity, Vector2Scale(player.dir, player.acceleration * (screenWidth / BASE_SCREEN_WIDTH) * GetFrameTime())),
-			{ -player.maxVelocity, -player.maxVelocity },
-			{ player.maxVelocity, player.maxVelocity });
+				{ -player.maxVelocity, -player.maxVelocity },
+				{ player.maxVelocity, player.maxVelocity });
 
-		if (player.position.x + player.velocity.x * GetFrameTime() > screenWidth)
-			player.position.x = 0;
-		else if (player.position.x + player.velocity.x * GetFrameTime() < 0)
-			player.position.x = screenWidth;
-
-		if (player.position.y + player.velocity.y * GetFrameTime() > screenHeight)
-			player.position.y = 0;
-		else if (player.position.y + player.velocity.y * GetFrameTime() < 0)
-			player.position.y = screenHeight;
-
+		CheckScreenBoundsCollision(player, screenWidth, screenHeight);
 	}
 
 	void PlayerDraw(Player& player)
@@ -45,5 +39,67 @@ namespace asteroids
 	void ResetPlayer(Player& player)
 	{
 		player.position = { static_cast<float>(GetScreenWidth()) / 2 - player.size / 2, static_cast<float>(GetScreenHeight()) / 2 - player.size / 2 };
+	}
+
+
+	void CheckScreenBoundsCollision(Player& player, float screenWidth, float screenHeight)
+	{
+		if (player.position.x + player.velocity.x * GetFrameTime() > screenWidth)
+		{
+			player.position.x = 0;
+			if (player.velocity.y > 0)
+			{
+				if (player.position.y > screenHeight / 2)
+					player.position.y = screenHeight - player.position.y;
+			}
+			else if (player.velocity.y < 0)
+			{
+				if (player.position.y < screenHeight / 2)
+					player.position.y = screenHeight - player.position.y;
+			}
+		}
+		else if (player.position.x + player.velocity.x * GetFrameTime() < 0)
+		{
+			player.position.x = screenWidth;
+			if (player.velocity.y > 0)
+			{
+				if (player.position.y > screenHeight / 2)
+					player.position.y = screenHeight - player.position.y;
+			}
+			else if (player.velocity.y < 0)
+			{
+				if (player.position.y < screenHeight / 2)
+					player.position.y = screenHeight - player.position.y;
+			}
+		}
+
+		if (player.position.y + player.velocity.y * GetFrameTime() > screenHeight)
+		{
+			player.position.y = 0;
+			if (player.velocity.x > 0)
+			{
+				if (player.position.x > screenWidth / 2)
+					player.position.x = screenWidth - player.position.x;
+			}
+			else if (player.velocity.x < 0)
+			{
+				if (player.position.x < screenWidth / 2)
+					player.position.x = screenWidth - player.position.x;
+			}
+		}
+		else if (player.position.y + player.velocity.y * GetFrameTime() < 0)
+		{
+			player.position.y = screenHeight;
+			if (player.velocity.x > 0)
+			{
+				if (player.position.x > screenWidth / 2)
+					player.position.x = screenWidth - player.position.x;
+			}
+			else if (player.velocity.x < 0)
+			{
+				if (player.position.x < screenWidth / 2)
+					player.position.x = screenWidth - player.position.x;
+			}
+		}
 	}
 }
