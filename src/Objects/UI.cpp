3086c25115e6212ffce73a99321a8dd37a2f2e23
Nicodragon25 <nicolasariellaure@gmail.com>
2023-10-30 16:10:57 -0300

    Update Rules Texts

--- src/Objects/UI.cpp
+++ src/Objects/UI.cpp
@@ -35,8 +35,9 @@ namespace asteroids
 		const char* rulesTitle = "Rules";
 		int titleSize = static_cast<int>(150 * GetScreenScale());
 		int rulesSize = static_cast<int>(40 * GetScreenScale());
-		const char* winConditionText = "Destroy as many viruses as you can to set a new HighScore";
-		const char* bloodCellsText = "But be careful, shooting at BloodCells might hurt the patient";
+		const char* winConditionText = "Destroy Blood Cells to weaken the target";
+		const char* whiteCellsText = "But be careful, white cells will damage you";
+		const char* whiteCellsText2 = "and some will pursue you";
 		const char* controlsTextLClick = "Left Click to Shoot";
 		const char* controlsTextMClick = "Middle Click to Pause";
 		const char* controlsTextRClick = "Right Click to Thrust";
@@ -44,8 +45,9 @@ namespace asteroids
 
 		DrawText(rulesTitle, GetScreenWidth() / 2 - MeasureText(rulesTitle, titleSize) / 2, titleWindowLimitSpacing, titleSize, WHITE);
 
-		DrawText(winConditionText, GetScreenWidth() / 2 - MeasureText(winConditionText, rulesSize) / 2, GetScreenHeight() / 2 - wordsSpacing, rulesSize, WHITE);
-		DrawText(bloodCellsText, GetScreenWidth() / 2 - MeasureText(bloodCellsText, rulesSize) / 2, GetScreenHeight(), rulesSize, WHITE);
+		DrawText(winConditionText, GetScreenWidth() / 2 - MeasureText(winConditionText, rulesSize) / 2, GetScreenHeight() / 2 - wordsSpacing * 3, rulesSize, WHITE);
+		DrawText(whiteCellsText, GetScreenWidth() / 2 - MeasureText(whiteCellsText, rulesSize) / 2, GetScreenHeight() / 2 - wordsSpacing * 2, rulesSize, WHITE);
+		DrawText(whiteCellsText2, GetScreenWidth() / 2 - MeasureText(whiteCellsText2, rulesSize) / 2, GetScreenHeight() / 2 - wordsSpacing, rulesSize, WHITE);
 		DrawText(controlsTextLClick, GetScreenWidth() / 2 - MeasureText(controlsTextLClick, rulesSize) / 2, GetScreenHeight() / 2 + wordsSpacing, rulesSize, WHITE);
 		DrawText(controlsTextMClick, GetScreenWidth() / 2 - MeasureText(controlsTextMClick, rulesSize) / 2, GetScreenHeight() / 2 + wordsSpacing * 2, rulesSize, WHITE);
 		DrawText(controlsTextRClick, GetScreenWidth() / 2 - MeasureText(controlsTextRClick, rulesSize) / 2, GetScreenHeight() / 2 + wordsSpacing * 3, rulesSize, WHITE);
